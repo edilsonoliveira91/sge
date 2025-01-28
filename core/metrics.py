f4fdb08714117ad2a91d1a4a9a9580bc12c976ee
Edilson Oliveira <edilsonoliveira@me.com>
2025-01-28 12:20:06 -0300

    Finished Graphics

--- core/metrics.py
+++ core/metrics.py
@@ -2,7 +2,9 @@ from django.db.models import Sum, F
 from django.utils import timezone
 from django.utils.formats import number_format
 from products.models import Product
+from brands.models import Brand
 from outflows.models import Outflow
+from categories.models import Category
 
 
 def get_product_metrics():
@@ -48,3 +50,25 @@ def get_daily_sales_data():
     values.append(float(sales_total))
   
   return dict(dates=dates, values=values)
+
+
+def get_daily_sales_quantity_data():
+  today = timezone.now().date()
+  dates = [str(today - timezone.timedelta(days=i)) for i in range(6, -1, -1)]
+  quantities = list()
+
+  for date in dates:
+    sales_quantity = Outflow.objects.filter(created_at__date=date).count()
+    quantities.append(sales_quantity)
+
+  return dict(dates=dates, values=quantities)
+
+
+def get_product_count_by_category():
+  categories = Category.objects.all()
+  return {category.name: Product.objects.filter(category=category).count() for category in categories}
+
+
+def get_graphic_product_brand_metric():
+  brands = Brand.objects.all()
+  return {brand.name: Product.objects.filter(brand=brand).count() for brand in brands}
--- core/views.py
+++ core/views.py
@@ -8,5 +8,8 @@ def home(request):
         'product_metrics': metrics.get_product_metrics(),
         'sales_metrics': metrics.get_sales_metrics(),
         'daily_sales_data': json.dumps(metrics.get_daily_sales_data()),
+        'daily_sales_quantity_data': json.dumps(metrics.get_daily_sales_quantity_data()),
+        'product_count_by_category': json.dumps(metrics.get_product_count_by_category()),
+        'product_count_by_brand': json.dumps(metrics.get_graphic_product_brand_metric()),
     }
     return render(request, 'home.html', context)
